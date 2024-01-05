1f70fad968583397d730f4ab5f48a47bf6b10264
wilchett <mitch.webb@att.net>
2024-01-05 17:26:26 -0600

    removed overlapping katydids, finished first version of overlays

--- src/components/PresetInfoButton.js
+++ src/components/PresetInfoButton.js
@@ -37,8 +37,23 @@ const PresetInfoButton = ({ itemDict }) => {
                         <img className='preset-image' src={`./images/${itemDict.image}`}/>
                         <div className='preset-summary-text'>
                             <h2>{itemDict.fullName}</h2>
+                            <i>{itemDict.scientificName}</i>
                         </div>
-                        <div className='preset-details-text'>heyo</div>
+                        <div className='preset-details-text'>
+                            <div className='attribution-chunk'>
+                                <div> Image: </div>
+                                <a target='_blank' href={itemDict.image_attribution_link}>
+                                    {itemDict.image_attribution_text}
+                                </a>
+                            </div>
+                            <div className='attribution-chunk'>
+                                <div> Audio: </div>
+                                <a target='_blank' href={itemDict.audio_attribution_link}>
+                                    {itemDict.audio_attribution_text}
+                                </a>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             )}
